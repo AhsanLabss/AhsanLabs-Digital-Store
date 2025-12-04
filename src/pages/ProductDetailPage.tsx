cfae428e79d5b1ed719daec0e4425b23f833c297
AhsanLabss <jodoffcl2@gmail.com>
2025-12-04 15:19:05 +0500

    🌀 Auto commit: ProductDetailPage.tsx

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -134,7 +134,7 @@ export const ProductDetailPage = ({ productId, onBack }: ProductDetailPageProps)
     } else {
       message = `${product.whatsappMessage} (Price: $ ${product.price.toFixed(2)})`;
     }
-    const whatsappNumber = '92335019';
+    const whatsappNumber = '923350194193';
     const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
   };
